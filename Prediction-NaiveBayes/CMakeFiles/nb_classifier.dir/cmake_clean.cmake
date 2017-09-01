2079f63a5b58550fd9bb2b2c450fc61c0bcd346a
PhilippeW <philippe.weingertner@gmail.com>
2017-09-01 18:32:57 +0200

    first commit

---
+++ Prediction-NaiveBayes/CMakeFiles/nb_classifier.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nb_classifier.dir/main.cpp.o"
+  "CMakeFiles/nb_classifier.dir/classifier.cpp.o"
+  "nb_classifier.pdb"
+  "nb_classifier"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nb_classifier.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
