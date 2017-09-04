bcaca3dffa5d210b22f0b7ef4c74c51195bddd94
PhilippeW <philippe.weingertner@gmail.com>
2017-09-04 12:13:24 +0200

    Naive Bayes classifier: based on raw 4 features (84.4% accuracy)

--- Prediction-NaiveBayes/classifier.cpp
+++ Prediction-NaiveBayes/classifier.cpp
@@ -5,6 +5,8 @@
 #include <vector>
 #include "classifier.h"
 
+#include <assert.h>
+
 /**
  * Initializes GNB
  */
@@ -33,6 +35,52 @@ void GNB::train(vector<vector<double>> data, vector<string> labels)
 		labels - array of N labels
 		  - Each label is one of "left", "keep", or "right".
 	*/
+
+  assert(data.size() == labels.size());
+
+  int num_labels = possible_labels.size();
+  int num_vars = data[0].size();
+
+  vector<int> num(num_labels, 0);
+
+  mu.resize(num_labels, vector<double>(num_vars));
+  for (auto& sub : mu) {
+      std::fill(sub.begin(), sub.end(), 0);
+  }
+
+  sigma.resize(num_labels, vector<double>(num_vars));
+  for (auto& sub : sigma) {
+      std::fill(sub.begin(), sub.end(), 0);
+  }
+
+  proba_class.reserve(num_labels);
+
+  for (size_t i = 0; i < data.size(); i++) {
+    for (size_t label = 0; label < num_labels; label++) {
+      if (possible_labels[label] == labels[i]) {
+        num[label]++;
+        for (size_t var = 0; var < num_vars; var++) {
+          mu[label][var] += data[i][var];
+          sigma[label][var] += pow(data[i][var], 2);
+        }
+        continue;
+      }
+    }
+  }
+
+  for (size_t label = 0; label < num_labels; label++) {
+    for (size_t var = 0; var < num_vars; var++) {
+      mu[label][var] /= num[label];
+      sigma[label][var] /= num[label];
+      sigma[label][var] -= pow(mu[label][var], 2);
+      sigma[label][var] = sqrt(sigma[label][var]);
+      cout << "label: " << label << " var: " << var << " mu=" << mu[label][var] << " sigma=" << sigma[label][var] << endl;
+    }
+    proba_class[label] = (double)num[label] / (double)data.size();
+    cout << "proba_class=" << proba_class[label] << endl;
+  }
+  
+
 }
 
 string GNB::predict(vector<double> sample)
@@ -54,6 +102,29 @@ string GNB::predict(vector<double> sample)
 		# TODO - complete this
 	*/
 
-	return this->possible_labels[1];
+  int num_labels = possible_labels.size();
 
+  vector<double> llh(num_labels, 0); // likelihood
+  double llh_max = 0;
+  int llh_label = 0;
+
+  // Apply Bayes rule
+  for (size_t label = 0; label < num_labels; label++) {
+    llh[label] = proba_class[label];
+    for (size_t var = 0; var < sample.size(); var++) {
+      llh[label] *= normpdf(sample[var], mu[label][var], sigma[label][var]);
+    }
+    if (llh[label] > llh_max) {
+      llh_max = llh[label];
+      llh_label = label;
+    }
+  }
+
+	//return this->possible_labels[1];
+	return this->possible_labels[llh_label];
+
+}
+
+double GNB::normpdf(double x, double mu, double sigma) { 
+    return  (1.0 / (sigma * sqrt(2.0 * M_PI))) * exp(-0.5 * pow((x - mu) / sigma, 2));
 }
--- Prediction-NaiveBayes/classifier.h
+++ Prediction-NaiveBayes/classifier.h
@@ -28,6 +28,14 @@ public:
 
   string predict(vector<double>);
 
+private:
+  double normpdf(double x, double mu, double sigma);
+
+  // typically here 3 classes and 4 variables
+  vector<double> proba_class;   // 3x1
+  vector<vector<double>> mu;    // 3x4
+  vector<vector<double>> sigma; // 3x4
+
 };
 
 #endif
